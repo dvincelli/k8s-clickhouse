42b74bd8c460695c52b156831925834cd607beea
David Vincelli <david.vincelli@gmail.com>
2023-11-03 19:55:59 -0400

    Capture kind bootstrap

---
+++ kind/create-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+kind create cluster --name=clickhouse --config=cluster.yaml
+
+kubectl config use-context kind-clickhouse
+
+SNAPSHOTTER_VERSION=v2.0.1
+
+# Apply VolumeSnapshot CRDs
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+
+# Create snapshot controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
+
+git submodule init
+
+csi-driver-host-path/deploy/kubernetes-latest/deploy.sh
+
+kubectl apply -f csi-storageclass.yaml
+kubectl apply -f nginx-ingress.yaml
