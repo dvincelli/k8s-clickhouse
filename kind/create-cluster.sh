a733659335fedd93b47da89b8619bd8d60b049c0
David Vincelli <david.vincelli@gmail.com>
2023-11-07 13:30:04 -0500

    Add final config

--- kind/create-cluster.sh
+++ kind/create-cluster.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-kind create cluster --name=clickhouse --config=cluster.yaml
+KIND_VERSION="v1.27.3"
+SNAPSHOTTER_VERSION="v2.0.1"
+NGINX_INGRESS_VERSION="v1.8.2"
+
+kind create cluster --name=clickhouse --config=cluster.yaml --image kindest/node:${KIND_VERSION}
 
 kubectl config use-context kind-clickhouse
 
-SNAPSHOTTER_VERSION=v2.0.1
+# Create dynamically provisioned hostpath storage class.
+# To allow statefulsets to provision templated storage.
 
 # Apply VolumeSnapshot CRDs
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
@@ -16,8 +21,12 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snaps
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${SNAPSHOTTER_VERSION}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
 
 git submodule init
+git submodule update
 
 csi-driver-host-path/deploy/kubernetes-latest/deploy.sh
 
 kubectl apply -f csi-storageclass.yaml
-kubectl apply -f nginx-ingress.yaml
+
+# Create NGINX ingress so we can port-forward
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-${NGINX_INGRESS_VERSION}/deploy/static/provider/cloud/deploy.yaml
